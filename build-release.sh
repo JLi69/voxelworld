91c40f8b9add48a64194caf13700aec244531d58
JL <74738871+JLi69@users.noreply.github.com>
2025-01-03 17:59:13 -0600

    create shell script to build voxelworld release for linux

---
+++ build-release.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+VOXELWORLD_DIR=voxelworld-linux/
+
+# creates a linux release of voxelworld (package is in .tar.gz format)
+rm -rf release/
+mkdir release/$VOXELWORLD_DIR -p
+# Copy the binary over
+cp target/release/voxelworld release/$VOXELWORLD_DIR
+# Copy `cfg.impfile`
+cp cfg.impfile release/$VOXELWORLD_DIR
+# Copy assets/
+cp -r assets/ release/$VOXELWORLD_DIR
+cd release/ && tar -czf voxelworld-linux.tar.gz $VOXELWORLD_DIR
