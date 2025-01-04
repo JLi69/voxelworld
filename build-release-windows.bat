53e7e09ebad00e1fdd555ad116f190669ceb6cb7
jli <coderguy50@gmail.com>
2025-01-03 19:00:14 -0600

    update credits.txt and add script to build zip file for windows

---
+++ build-release-windows.bat
@@ -0,0 +1,14 @@
+@echo off
+Rem this creates a zip file with the voxelworld executable inside
+
+set VOXELWORLD_DIR=voxelworld-windows
+
+rmdir /S release\
+mkdir release\%VOXELWORLD_DIR%
+copy target\release\voxelworld.exe release\%VOXELWORLD_DIR%
+copy cfg.impfile release\%VOXELWORLD_DIR%
+Xcopy /E /I assets\ release\%VOXELWORLD_DIR%\assets\
+cd release\
+Rem NOTE: You need 7-zip installed to use this script
+"C:\Program Files\7-Zip\7z.exe" a -tzip voxelworld-linux.zip %VOXELWORLD_DIR%
+cd ..
